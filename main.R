553c52e485c335502d45c3995330373f8e0e504e
hey2homie <photocard4@gmail.com>
2021-03-03 14:12:06 +0100

    Add missing value visualization

--- main.R
+++ main.R
@@ -1,2 +1,26 @@
+library(tidyverse)
+
 dataframe <- read.table("./muscle-incomplete.txt", header = TRUE)
+
+# Exploring missing data by visual aids
+missing_values <- dataframe %>%
+  	gather(key = "key", value = "val") %>%
+  	mutate(isna = is.na(val)) %>%
+  	group_by(key) %>%
+	mutate(total = n()) %>%
+	group_by(key, total, isna) %>%
+	summarise(num.isna = n()) %>%
+	mutate(pct = num.isna / total * 100)
+
+percentage.plot <- ggplot() +
+	geom_bar(data = missing_values, aes(x = reorder(key, desc(pct)), y = pct, fill = isna), stat = 'identity',
+			 alpha = 0.9) +
+	scale_fill_manual(name = "", values = c('steelblue', 'tomato3'), labels = c("Present", "Missing")) +
+	coord_flip() +
+	labs(title = "Percentage of missing values", x = 'Variable', y = "% of missing values") +
+	theme_classic() +
+	theme(plot.title = element_text(hjust = 0.5))
+
+percentage.plot
+
 
